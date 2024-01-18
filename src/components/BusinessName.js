7c87061f5b3266cb385fa1d4c6f4467f4157ae0b
Lavender <lavendersanders88@yahoo.com>
2024-01-17 21:18:29 -0600

    added hours to landing page

---
+++ src/components/BusinessName.js
@@ -0,0 +1,7 @@
+export default function BusinessName() {
+    return(
+        <div>
+            <p>Lovie's Nails</p>
+        </div>
+    );
+}
---
+++ src/components/Hours.js
@@ -0,0 +1,16 @@
+export default function Hours() {
+    return(
+        <div>
+            <h2>Hours of Operation</h2>
+            <ul>
+                <li>Monday 4pm to 8pm</li>
+                <li>Tuesday 4pm to 8pm</li>
+                <li>Wednesday 4pm to 8pm</li>
+                <li>Thursday 4pm to 8pm</li>
+                <li>Friday 4pm to 8pm</li>
+                <li>Saturday 10am to 8pm</li>
+                <li>Sunday</li>
+            </ul>
+        </div>
+    );
+}
