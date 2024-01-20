e2bafe662f581ea22c66d908b5046741b3e6e4b1
Terri Penn <coderterri@protonmail.com>
2024-01-19 21:00:29 -0600

    merge Lavender's branch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./styling.css";
 import Navbar from "./components/Navbar";
+import Contact from './components/Contact';
+import Hours from './components/Hours';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+      <div className="App">
+          <Navbar/>
+          <header>
+              <Hours/>
+              <Contact/>
+          </header>
+      </div>
   );
 }
 
