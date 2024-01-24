1b8f5af5d15faa0c8434158cc873a5ff4c459fd7
Terri Penn <coderterri@protonmail.com>
2024-01-24 16:06:51 -0600

    change Services.js to a function component for consistency with other components

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,53 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Table } from 'reactstrap';
 
-class ServicesList extends Component {
+export default function ServicesList() {
+    const [services, setServices] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    constructor(props) {
-        super(props);
-        this.state = {services: []};
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         fetch('http://localhost:8080/api/v1/services')
             .then(response => response.json())
-            .then(data => this.setState({services: data}));
+            .then(data => {
+                setServices(data);
+                setIsLoading(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading...</p>;
     }
-    render() {
-        const {services, isLoading} = this.state;
 
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
+    const servicesList = services.map(service => (
+        <tr key={service.id}>
+            <td style={{ whiteSpace: 'nowrap' }}>{service.name}</td>
+            <td>{service.description}</td>
+            <td>{service.price}</td>
+        </tr>
+    ));
 
-        const servicesList = services.map(service => {
-            return <tr key={service.id}>
-                <td style={{whiteSpace: 'nowrap'}}>{service.name}</td>
-                <td>{service.description}</td>
-                <td>{service.price}</td>
-            </tr>
-        });
-
-        return (
-            <div>
-                <Container fluid>
-                    <h3>Services</h3>
-                    <Table className="mt-4">
-                        <thead>
-                        <tr>
-                            <th width="30%">Service</th>
-                            <th width="30%">Description</th>
-                            <th width="40%">Price</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {servicesList}
-                        </tbody>
-                    </Table>
-                </Container>
-            </div>
-        );
-    }
-}
-
-export default ServicesList;
+    return (
+        <div>
+            <Container fluid>
+                <h3>Services</h3>
+                <Table className="mt-4">
+                    <thead>
+                    <tr>
+                        <th width="30%">Service</th>
+                        <th width="30%">Description</th>
+                        <th width="40%">Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>{servicesList}</tbody>
+                </Table>
+            </Container>
+        </div>
+    );
+};
