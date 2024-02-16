ddb696c879211a4580876625a3916f488c3d030f
Terri Penn <coderterri@protonmail.com>
2024-02-15 20:53:31 -0600

    clean up Services API example and pretty it up

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Table } from 'reactstrap';
+import { Button, Container, Card, Col, Row, Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ServicesList() {
     const [services, setServices] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+    const getServices = () => {
         fetch('http://localhost:8080/api/v1/services')
             .then(response => response.json())
             .then(data => {
                 setServices(data);
                 setIsLoading(false);
-            });
+            })
+            .catch(error => console.error(error));
+    }
+    const removeService = (id) => {
+        fetch('http://localhost:8080/api/v1/services/' + id, {'method': 'DELETE'})
+            .then(response => {
+                if (!response.ok) {
+                    console.error('Service delete: network error');
+                } else {
+                    getServices();
+                }
+            })
+            .catch(error => console.error(error));
+    }
+    useEffect(() => {
+        getServices()
     }, []);
 
     if (isLoading) {
@@ -23,24 +39,38 @@ export default function ServicesList() {
             <td style={{ whiteSpace: 'nowrap' }}>{service.name}</td>
             <td>{service.description}</td>
             <td>{service.price}</td>
+            <td>
+                <Button className='btn btn-danger' onClick={ () => removeService(service.id)}>
+                    Delete
+                </Button>
+            </td>
         </tr>
     ));
 
     return (
         <div>
-            <Container fluid>
-                <h3>Services</h3>
-                <Table className="mt-4">
-                    <thead>
-                    <tr>
-                        <th width="30%">Service</th>
-                        <th width="30%">Description</th>
-                        <th width="40%">Price</th>
-                    </tr>
-                    </thead>
-                    <tbody>{servicesList}</tbody>
-                </Table>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Header>Services</Card.Header>
+                            <Card.Body>
+                                <Table striped bordered hover>
+                                    <thead>
+                                    <tr>
+                                        <th>Service</th>
+                                        <th>Description</th>
+                                        <th>Price</th>
+                                        <th>Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>{servicesList}</tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
-};
+}
